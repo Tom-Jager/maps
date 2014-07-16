9f24f4615797b5b9143e2117088ec6d5a694e33d
Scott Street <scottystreet@Scotts-MacBook-Air.local>
2014-07-16 11:34:37 +0100

    put points into array properly again #2

--- main.js
+++ main.js
@@ -11,37 +11,37 @@ function init() {
   var loader = new FacebookLoader();
   loader.setFriendsLimit(5);
   loader.run(function () {
-    var friends = profile.getFriends();
+    var friends = my_profile.getFriends();
     plotFriends(friends, map);
   });
 }
 
 function plotFriends(friends, map) {
   var geo = new google.maps.Geocoder;
-  var name, hometown;
+  var name, hometown, point, infowindow;
 
   for(var i = 0; i < friends.length; i++) {
-    name = friends[i].getName();
-    if(friends[i].getHometown() !== null) {
 
+    if(friends[i].getHometown() !== null) {
+      name = friends[i].getName();
       hometown = friends[i].getHometown().getName();
 
       geo.geocode({ 'address': hometown }, function(res, status) {
 
         if(status == google.maps.GeocoderStatus.OK) {
 
-          points[i] = new google.maps.Marker({
+          point = new google.maps.Marker({
             position: res[0].geometry.location,
             map: map,
             title: name
           });
 
-          infowindows[i] = new google.maps.InfoWindow({
+          infowindow = new google.maps.InfoWindow({
             content: hometown
           });
 
-          google.maps.event.addListener(points[i], 'click', function() {
-            infowindows[i].open(map, points[i]);
+          google.maps.event.addListener(point, 'click', function() {
+            infowindow.open(map, point);
           });
 
         }
