8bf55f27f9e3e3616845e6b9b0f4c884aff34d8a
Scott Street <scottystreet@Scotts-MacBook-Air.local>
2014-07-16 09:40:20 +0100

    changes

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-function initialize() {
+function init() {
   var map = new google.maps.Map(document.getElementById("map-canvas"), {
     center: new google.maps.LatLng(0, 0),
     zoom: 2,
     mapTypeId: google.maps.MapTypeId.HYBRID
   });
 
-  plotPoint("", map); //TODO: pass fb data TEST
-}
-
-var friendsLoc = new Array();
-
-function runLoader() {
-  // Create an instance of FacebookLoader
-  // and store it in a variable called loader
   var loader = new FacebookLoader();
   loader.setFriendsLimit(20);
-  loader.run(function () {
+  loader.run(function (my_profile) {
     var friends = my_profile.getFriends();
-    for (var i=0; i<friends.length; i++) {
-      friendsLoc[i][0] = friends[i].getName();
-      friendsLoc[i][1] = friends[i].getHometown();
-      friendsLoc[i][2] = friendsLoc[i][1].getId();
-      friendsLoc[i][1] = friendsLoc[i][1].getName();
-    }
+    plotFriends(friends, map);
   });
 }
 
-function plotPoint(data, map) {
+function plotFriends(friends, map) {
+  for(var i = 0; i < friends.length; i++) {
+    plotFriend(friends[i], map);
+  }
+}
+
+function plotFriend(friend, map) {
+  var name = friend.getName();
+  var hometown = friend.getHometown().getName();
+
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(51.5072, 0.1275), //TODO: get long lat from fb hometown
     map: map,
-    title:""
+    title: name
   });
 
   var infowindow = new google.maps.InfoWindow({
-    content: "" //TODO: get info from fb
+    content: hometown
   });
 
   google.maps.event.addListener(marker, 'click', function() {
@@ -42,4 +38,4 @@ function plotPoint(data, map) {
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', init); //now done onload body
